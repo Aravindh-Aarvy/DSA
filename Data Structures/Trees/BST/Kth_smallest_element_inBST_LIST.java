cc1ab5c5885d99eac801f0d7933a46c48f3d43dd
Aravindh Aarvy <87980815+Aravindh-Aarvy@users.noreply.github.com>
2025-06-25 23:14:44 +0530

    Create Kth_smallest_element_inBST_LIST.java

---
+++ Data Structures/Trees/BST/Kth_smallest_element_inBST_LIST.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Kth_smallest_element_inBST_LIST {
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> list  = new ArrayList<>();
+        inOrder(root,list);
+        return list.get(k-1);
+    }
+    public void inOrder(TreeNode node,List<Integer> list){
+        if(node == null) return;
+        
+        inOrder(node.left,list);
+        list.add(node.val);
+        inOrder(node.right,list);
+
+    }
+}
