53ad6a2d1c4af34e8aa81ae96e0fda30ab6a53d7
Aravindh Aarvy <87980815+Aravindh-Aarvy@users.noreply.github.com>
2025-06-26 08:33:00 +0530

    Update Insertion.java

--- Data Structures/Trees/BST/Insertion.java
+++ Data Structures/Trees/BST/Insertion.java
@@ -48,3 +48,15 @@ class Solution {
 
     }
 }
+
+
+// -- another way --
+
+class Solution2 {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null) return new TreeNode(val);
+        if(root.val>val){ root.left =insertIntoBST(root.left, val); }
+        else {root.right = insertIntoBST(root.right, val);}
+        return root;
+    }
+}
